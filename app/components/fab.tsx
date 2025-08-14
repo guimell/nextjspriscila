c345e7b4cbd717c61033fde6c4a0f2489f84c29b
guimell <guimell11@hotmail.com>
2025-08-14 14:29:08 -0300

    enhance: add transition effects to FAB button

--- app/components/fab.tsx
+++ app/components/fab.tsx
@@ -4,7 +4,7 @@ import Whatsapp from "./svg";
 export default function Fab() {
   return (
     <Link href="https://wa.me/5571981618308?text=Olá%20P2P!">
-      <div className="fixed bottom-4 right-4 bg-green-600 p-4 rounded-full shadow-lg ">
+      <div className="fixed bottom-4 right-4 rounded-full bg-green-600 p-4 shadow-lg transition-all duration-300 ease-in-out hover:animate-bounce hover:bg-green-700">
         <Whatsapp />
       </div>
     </Link>
